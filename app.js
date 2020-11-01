4b10a61e6490688d46b611aafd9d241f82ff1515
Mitchell Gams <mitchgams@gmail.com>
2020-11-01 01:53:44 -0600

    I don't think I can improve this further

--- app.js
+++ app.js
@@ -2,13 +2,9 @@ let dice = [];
 let unicode = ['\u2680', '\u2681', '\u2682', '\u2683', '\u2684', '\u2685'];
 let dieCount = 0;
 $('document').ready(() => {
-    $('#newDie').click(() => {
-        dice.push(new Die());
-    });
+    $('#newDie').click(() => dice.push(new Die()));
     $('#rerollDie').click(() => {
-        for(die of dice) {
-            die.reroll();
-        }
+        for(die of dice) die.reroll();
     });
     $('#sumDie').click(() => {
             let sum = 0;
@@ -19,39 +15,25 @@ $('document').ready(() => {
 class Die {
     constructor() {
         this.value = this.roll();
-        //console.log(this);
     }
 
     roll() {
         let possibility = Math.floor(Math.random() * 6) + 1;
-        $('.flex-dice').append(`<div class='${dieCount}' id='${possibility}'>${unicode[possibility-1]}</div>`);
-        this.count = dieCount;
-        dieCount++;
-        this.dieFlex = $(`.${this.count}`);
-        this.dieFlex.click(() => this.reroll());
-        this.dieFlex.dblclick(() => this.delete());
+        this.newItem = $(`<div>${unicode[possibility-1]}</div>`);
+        $('.flex-dice').append(this.newItem);
+        this.newItem.click(() => this.reroll());
+        this.newItem.dblclick(() => this.delete());
         return possibility;
     }
 
     delete() {
-        //dice.splice(this.count, 1);
-        /******************************************************
-         * I tried removing the object from the array but the way
-         * that I wrote this, the index position in the array
-         * is important. So I decided to leave it in there and
-         * just make it be ignored when the value is set to 0.
-         ******************************************************/
-        this.value = 0;
-        this.dieFlex.remove();
+        dice.splice(dice.indexOf(this), 1);
+        this.newItem.remove();
     }
 
     reroll() {
-        if(this.value === 0) return; // deleted die
-        let possibility = Math.floor(Math.random() * 6) + 1;
-        this.dieFlex.empty();
-        this.dieFlex.append(unicode[possibility-1]);
-        this.dieFlex.attr('id', possibility);
-        this.value = possibility;
+        this.value = Math.floor(Math.random() * 6) + 1;
+        this.newItem.empty().append(unicode[this.value-1]);
     }
     
 }
